8b37c2eceeb724b3c6e8a514f306eebd4edac90b
root <root@saasbook.(none)>
2013-07-08 06:40:47 -0700

    try 2

--- web.js
+++ web.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var fs = require('fs');
+var buff = new Buffer(64);
+var len = nulll;
+
+fs.readFile('index.html', 'utf8', function (err, data) {
+    if (err) {
+	len = buff.write("there was a file-read error");
+    }
+    len = buff.write(data);
+});
 
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
-    var buff = new Buffer(64);
-    var len = buff.write(fs.readFile('index.html'));    
     response.send(buff.toString('utf8', 0, len));
 });
 
